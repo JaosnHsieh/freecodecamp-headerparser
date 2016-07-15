702b13caaee777997c1106363c6e8d30954f8b17
jasonHsieh <appleoxxo@gmail.com>
2016-07-15 10:36:33 +0800

    '3nd'

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var app = express();
 
+app.enable('trust proxy');
 app.get('/',function(req,res){
-    res.send('my header parser');
+    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+    var lang = req.headers['accept-language'];
+    lang = lang.split(',')[0];
+    var os = req.headers['user-agent'].split(' ')[1].substring(1);
+    var obj = {ip:ip,language:lang,os:os};
+    res.json(obj);
 });
 
 app.listen(process.env.PORT || 8080);
